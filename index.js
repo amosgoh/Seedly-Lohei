54b51fb8ccd75912513569580053e1433a3a32cf
amosgoh <gohjiashengamos@gmail.com>
2018-02-12 13:26:42 +0800

    Added trial JS code for disable scrolling

--- index.js
+++ index.js
@@ -97,4 +97,22 @@ function generate4d() {
 }
 
 
+var disableScroll = false;
+var scrollPos = 0;
+function stopScroll() {
+    disableScroll = true;
+    scrollPos = $(window).scrollTop();
+}
+function enableScroll() {
+    disableScroll = false;
+}
+$(function(){
+    $(window).bind('scroll', function(){
+         if(disableScroll) $(window).scrollTop(scrollPos);
+    });
+    $(window).bind('touchmove', function(){
+         $(window).trigger('scroll');
+    });
+});
+
 generate4d();

871334a312499560072b73a12c9b1c7f53097aec
amosgoh <gohjiashengamos@gmail.com>
2018-02-13 11:37:29 +0800

    Added color to Shares + cleaned up JS

--- index.js
+++ index.js
@@ -97,23 +97,4 @@ function generate4d() {
   document.getElementById("luckyNumber").innerHTML = luckyNumber;
 }
 
-
-var disableScroll = false;
-var scrollPos = 0;
-function stopScroll() {
-    disableScroll = true;
-    scrollPos = $(window).scrollTop();
-}
-function enableScroll() {
-    disableScroll = false;
-}
-$(function(){
-    $(window).bind('scroll', function(){
-         if(disableScroll) $(window).scrollTop(scrollPos);
-    });
-    $(window).bind('touchmove', function(){
-         $(window).trigger('scroll');
-    });
-});
-
 generate4d();
